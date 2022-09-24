6163bec765b9c1794453a9d8c8d54988c8b67166
Fdev808 <a66428168@gmail.com>
2022-09-24 07:51:40 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,55 @@
+/**
+   * Create By Dika Ardnt.
+   * Recode By Naze Dev
+   * Contact Me on wa.me/6282113821188
+   * Follow https://github.com/nazedev
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': '0e92565522',
+}
+
+// Other
+global.owner = ['6283811034750']
+global.ownernomer = "6283811034750"
+global.premium = ['6283811034750']
+global.packname = 'Sticker  Gratis'
+global.author = 'Fdev'
+global.sessionName = 'session'
+global.prefa = ['','!','.','#','&']
+global.sp = ''
+global.mess = {
+    success: 'Sucess',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    premime: 'Fitur Khusus Premium Kalo Mau Daftar Ketik Sewa',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Lagi aku proses ya kak sabar...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 250
+}
+global.thumb = fs.readFileSync('./lib/shiki.png')
+global.vaze = { url: 'https://telegra.ph/file/f80af7befdbe5537f5046.mp4' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
